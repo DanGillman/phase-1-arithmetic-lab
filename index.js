5188747a504fb0f9d5092cd4dc8854f482099ec4
dangillman <dangillman93@gmail.com>
2024-01-23 12:39:38 -0500

    added content to index.js, passed tests.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 1;
+let num2 = 62;
 
-// Write your code here
+let multiply = num1 * num2;
+console.log(multiply); // Output: 62
+let random = Math.floor(Math.random() * 100) + 1;
+let num3 = 11;
+let num4 = 7;
+
+let mod = num3 % num4;
+
+let max = Math.max(18, 19, 20);
+console.log(max); // Output: 20
